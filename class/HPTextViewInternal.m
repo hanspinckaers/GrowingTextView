2a16c89b27b5173cdcefb48630b5c45e3f05a19b
Hani Shabsigh <hanishabsigh@gmail.com>
2013-10-18 18:46:00 -0400

    Added setPlaceholder method to call setNeedsDisplay

--- class/HPTextViewInternal.m
+++ class/HPTextViewInternal.m
@@ -30,10 +30,6 @@
 
 @implementation HPTextViewInternal
 
-@synthesize placeholder;
-@synthesize placeholderColor;
-@synthesize displayPlaceHolder;
-
 -(void)setText:(NSString *)text
 {
     BOOL originalValue = self.scrollEnabled;
@@ -101,15 +97,22 @@
 - (void)drawRect:(CGRect)rect
 {
     [super drawRect:rect];
-    if (displayPlaceHolder && placeholder && placeholderColor)
+    if (self.displayPlaceHolder && self.placeholder && self.placeholderColor)
     {
 #ifdef __IPHONE_7_0
-        [placeholder drawInRect:CGRectMake(5, 8 + self.contentInset.top, self.frame.size.width-self.contentInset.left, self.frame.size.height- self.contentInset.top) withAttributes:@{NSFontAttributeName:self.font, NSForegroundColorAttributeName:self.placeholderColor}];
+        [self.placeholder drawInRect:CGRectMake(5, 8 + self.contentInset.top, self.frame.size.width-self.contentInset.left, self.frame.size.height- self.contentInset.top) withAttributes:@{NSFontAttributeName:self.font, NSForegroundColorAttributeName:self.placeholderColor}];
 #else
-        [placeholderColor set];
-        [placeholder drawInRect:CGRectMake(8.0f, 8.0f, self.frame.size.width - 16.0f, self.frame.size.height - 16.0f) withFont:self.font];
+        [self.placeholderColor set];
+        [self.placeholder drawInRect:CGRectMake(8.0f, 8.0f, self.frame.size.width - 16.0f, self.frame.size.height - 16.0f) withFont:self.font];
 #endif
     }
+}
+
+-(void)setPlaceholder:(NSString *)placeholder
+{
+	_placeholder = placeholder;
+	
+	[self setNeedsDisplay];
 }
 
 @end
