13358731ecdc823f6cdc02297c3b07d131087c25
Arnas Dundulis <arnas@vinted.com>
2017-01-26 11:37:59 +0200

    Add framework target to test project

---
+++ example/HPGrowingTextViewFramework/HPGrowingTextViewUmbrella.h
@@ -0,0 +1,19 @@
+//
+//  HPGrowingTextView.h
+//  HPGrowingTextView
+//
+//  Created by Arnas Dundulis on 1/26/17.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HPGrowingTextView.
+FOUNDATION_EXPORT double HPGrowingTextViewVersionNumber;
+
+//! Project version string for HPGrowingTextView.
+FOUNDATION_EXPORT const unsigned char HPGrowingTextViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HPGrowingTextView/PublicHeader.h>
+#import <HPGrowingTextView/HPGrowingTextView.h>
+#import <HPGrowingTextView/HPTextViewInternal.h>
